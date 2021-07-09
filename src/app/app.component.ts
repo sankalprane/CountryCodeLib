5861ad18a472257ab333a83d38f670998157467e
unknown <sankalprane1998@gmail.com>
2021-07-09 13:31:09 +0530

    rought imple

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'temp2';
+export class AppComponent implements OnInit{
+
+  @ViewChild('f') Form: NgForm;
+
+  ngOnInit() {
+  }
+
+  getCode(number: string) {
+    console.log(number);
+  }
+
+  onSubmit() {
+    console.log(this.Form.value);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {Ng2TelInputModule} from 'ng2-tel-input';
+import {FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { CountrycodeComponent } from './countrycode/countrycode.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CountrycodeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    Ng2TelInputModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
